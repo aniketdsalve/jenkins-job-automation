8e837c1f12866b3b48e0e0daf80f9fd6314b73ee
Aniket Salve <69056191+aniketdsalve@users.noreply.github.com>
2023-04-21 20:26:24 +0530

    Update test.groovy

--- test.groovy
+++ test.groovy
@@ -1,5 +1,5 @@
 pipeline {
-    agent any
+	agent { label 'master' }
     parameters {
     choice(name: 'FOLDER_NAME', choices: ['OnBoardingDeveloperStack', 'Dev'], description: 'Select your Choice')
     string(name: 'JOB_NAME', defaultValue: 'DEMO-', description: 'Please enter Job Name.', trim: true)
@@ -14,18 +14,26 @@ pipeline {
                     
                     jobDsl sandbox: true, scriptText: '''freeStyleJob("${FOLDER_NAME}/${JOB_NAME}") {
                     	
-					logRotator {
+					    logRotator {
                             numToKeep(3)
                             daysToKeep(3)
                         }
 
                     	scm {
-                              github(\'git@bitbucket.org:sohesh/onborddeveloperstack.git\', \'*/dev\')
-                              
-                            }
+                          github(\'git@bitbucket.org:sohesh/onborddeveloperstack.git\', \'*/dev\')
+                          
+                        }
+                        wrappers {
+                            nodejs('NodeJS')
+                        }
+                        environmentVariables {
+                            keepBuildVariables(false)
+                            keepSystemVariables(false)
+                            env('Properties Content', 'PATH=$PATH:/var/lib/jenkins/.serverless/bin')
+                        }
 
                     	steps {
-                    		shell('cd onbrd_services/ \\nnpm install \\nserverless-package-python-functions \\nnpm install serverless-package-delete-loggroups \\nenv')
+                    		shell('cd ${PATH_DETAILS}/ \\nnpm install \\nserverless-package-python-functions \\nnpm install serverless-package-delete-loggroups \\nenv')
                     	}
                     
                     }'''
