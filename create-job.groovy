0f0e0dc8409768431cd64f7c77633acf32f95953
Aniket Salve <69056191+aniketdsalve@users.noreply.github.com>
2023-05-11 10:23:39 +0530

    Final code for seed job

---
+++ create-job.groovy
@@ -0,0 +1,42 @@
+pipeline {
+    agent any
+    parameters {
+    choice(name: 'FOLDER_NAME', choices: ['OnBoardingDeveloperStack', 'Dev'], description: 'Select your Choice')
+    string(name: 'JOB_NAME', defaultValue: 'DEMO-', description: 'Please enter Job Name.', trim: true)
+  }
+  
+
+    stages {
+        stage('Create Job') {
+            steps {
+                script {
+                    
+                    
+                    jobDsl sandbox: true, scriptText: '''freeStyleJob("${FOLDER_NAME}/${JOB_NAME}") {
+                    	
+					    logRotator {
+                            numToKeep(3)
+                            daysToKeep(3)
+                        }
+
+                    	scm {
+                          github(\'sohesh/onborddeveloperstack\', \'*/dev\', String protocol = \'ssh\', String host = \'bitbucket.org\')
+                          
+                        }
+                        wrappers {
+                            nodejs('NodeJS')
+                            environmentVariables {
+                                env(\'PATH\', \'$PATH:/var/lib/jenkins/.serverless/bin\')
+                            }
+                        }
+                        steps {
+                    		shell('cd onbrding_Service/ \\nnpm install \\nserverless-package-python-functions \\nnpm install serverless-package-delete-loggroups \\nenv')
+                    	}
+                    
+                    }'''
+					
+                }
+            }
+        }
+    }
+}
